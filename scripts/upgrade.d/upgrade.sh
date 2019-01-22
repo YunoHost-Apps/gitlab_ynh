41fb54152badbf572a3c12eb807d71bba20a453a
Bourre Pierre <pierre.bourre@inria.fr>
2019-01-22 15:53:43 +0100

    upgrade to 11.7.0

--- scripts/upgrade.d/upgrade.sh
+++ scripts/upgrade.d/upgrade.sh
@@ -1,5 +1,5 @@
-gitlab_version="11.6.3"
+gitlab_version="11.7.0"
 
-gitlab_x86_64_source_sha256="5a990a76c1656ddd42673562d8bd1be106a835331a2ce976d405c536a5567485"
+gitlab_x86_64_source_sha256="5a989b1e664494e6de139064b349fe9b22f37d5edd2918a4968fdf5e818a10c4"
 
-gitlab_arm_source_sha256="3b1d1c650df8722d0b63b5db3e6c2440ea70224deeca0621a9c5226a06079d2e"
+gitlab_arm_source_sha256="558e6b3cf87bd5c8b3dee567b2557ee6cc3fb40655cc6dabe5531c94cf27b99d"
