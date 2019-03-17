01bebbfda68ecea6a2c21c35cb574edb9952aa9d
Pierre Bourré <pierre.moltess@gmail.com>
2019-03-17 20:14:56 +0100

    Better name for the gitlab_filename

--- scripts/upgrade.d/upgrade.last.sh
+++ scripts/upgrade.d/upgrade.last.sh
@@ -4,4 +4,4 @@ gitlab_x86_64_source_sha256="ee670ddbad60bdd10d93749593a25a62f1d10f2b232da70b8e4
 
 gitlab_arm_source_sha256="582a5ed77ef50a16377a00f403a6b8f425e8fb98593be95fb0f7cb6db8e5629d"
 
-gitlab_filename="gitlab-ce.deb"
+gitlab_filename="gitlab-ce-${gitlab_version}.deb"
