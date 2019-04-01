0b304cb036c556872eb0556b48e73bb96dd717b1
Pierre Bourré <pierre.moltess@gmail.com>
2019-04-01 22:17:08 +0200

    Update 11.9.4

---
+++ scripts/upgrade.d/upgrade.11.9.1.sh
@@ -0,0 +1,7 @@
+gitlab_version="11.9.1"
+
+gitlab_x86_64_source_sha256="9668345176be07f1f8637b3c72ffea6f8fe2e76a8c5b10b74a340b9b6b97788c"
+
+gitlab_arm_source_sha256="df1fb5be8ec089a77cfe8276cbaabe0e40b0f3368cafc07de984d7795252ee21"
+
+gitlab_filename="gitlab-ce-${gitlab_version}.deb"
