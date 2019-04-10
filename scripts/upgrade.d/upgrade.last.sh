71300998aa9819aae1c8394e30921b8b9e0e3c46
Kayou <pierre.moltess@gmail.com>
2019-04-11 01:14:53 +0200

    Update gitlab to 11.9.7

--- scripts/upgrade.d/upgrade.last.sh
+++ scripts/upgrade.d/upgrade.last.sh
@@ -1,7 +1,7 @@
-gitlab_version="11.9.4"
+gitlab_version="11.9.7"
 
-gitlab_x86_64_source_sha256="20963cd803b116d041795f50c607ddf837e7226bc46025997348fe1c2e5e35ca"
+gitlab_x86_64_source_sha256="6851e1ee26250962740be5be6063dc536b7136237a2f8bb91df365a113bf536e"
 
-gitlab_arm_source_sha256="dba6f34b23ba74765b03ce9521b16a5e951e6bd6d1f2fdcce692a58f43a4c48b"
+gitlab_arm_source_sha256="b787bfba2a29f596f97ea39e2ed6c1b8e386d0cbdc043ffae515fe6e79914413"
 
 gitlab_filename="gitlab-ce-${gitlab_version}.deb"
