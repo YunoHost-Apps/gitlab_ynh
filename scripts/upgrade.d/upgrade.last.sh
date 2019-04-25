aadc0efbdfcbf6e9d5636274af107b6bb9081d9d
kay0u <pierre.moltess@gmail.com>
2019-04-25 17:21:31 +0200

    Upgrade gitlab to 11.10.1

--- scripts/upgrade.d/upgrade.last.sh
+++ scripts/upgrade.d/upgrade.last.sh
@@ -1,7 +1,7 @@
-gitlab_version="11.10.0"
+gitlab_version="11.10.1"
 
-gitlab_x86_64_source_sha256="6b642e84bf7beebc12da0aa97fdfb939bb0850b61d79e8f727bee172c176b93f"
+gitlab_x86_64_source_sha256="cffde1c3875c2864c3e954dd1077acde3cc87d8f8963309d2ac4455d621748eb"
 
-gitlab_arm_source_sha256="64b98ea0440c479dc3f2372bac9b7b57cc29f360c2ba7e801499766810e584a9"
+gitlab_arm_source_sha256="f414dbfaa074843c0f693e8d5cd63d6fd913c7a39358ebcb55f5be664aaf5916"
 
 gitlab_filename="gitlab-ce-${gitlab_version}.deb"
