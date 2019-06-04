9813b75f7dfa053d70b44faf442ce7d1e0be8583
Kayou <pierre.moltess@gmail.com>
2019-06-04 09:52:23 +0200

    Upgrade to 11.11.1

--- scripts/upgrade.d/upgrade.last.sh
+++ scripts/upgrade.d/upgrade.last.sh
@@ -1,7 +1,7 @@
-gitlab_version="11.11.0"
+gitlab_version="11.11.1"
 
-gitlab_x86_64_source_sha256="e15df41f1a9d40d23a710352ce1a77b30ebdd075dbc92430aa49dd6c3107f5c6"
+gitlab_x86_64_source_sha256="b23af7ec2689e6eff69927e1176e8b009ed01b6b66df4ff80e9ab5da67e0684d"
 
-gitlab_arm_source_sha256="03ce2b720e7266fb5ea6e00b3dc4f71b0c886c336db0669422ec332fe4df4447"
+gitlab_arm_source_sha256="4847ae487dcbcb26cf60842cd016b66ccccc5a1e71778582d333010aa0daf7cf"
 
 gitlab_filename="gitlab-ce-${gitlab_version}.deb"
