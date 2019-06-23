17b992442968c4c07357525d4234b4d1873c7587
Pierre Bourré <pierre.moltess@gmail.com>
2019-06-23 14:11:52 +0200

    Update Gitlab to 12.0.0

--- scripts/upgrade.d/upgrade.last.sh
+++ scripts/upgrade.d/upgrade.last.sh
@@ -1,7 +1,7 @@
-gitlab_version="11.11.1"
+gitlab_version="12.0.0"
 
-gitlab_x86_64_source_sha256="b23af7ec2689e6eff69927e1176e8b009ed01b6b66df4ff80e9ab5da67e0684d"
+gitlab_x86_64_source_sha256="b32070d2a4c83c914b9fd07713501e3c99e31a288de64a7794a6de2c2b6f7494"
 
-gitlab_arm_source_sha256="4847ae487dcbcb26cf60842cd016b66ccccc5a1e71778582d333010aa0daf7cf"
+gitlab_arm_source_sha256="cd3f0ffd16d1d80184e79b552ae5cc3bb33012d1d51bb9c5cb48d461f5c70b07"
 
 gitlab_filename="gitlab-ce-${gitlab_version}.deb"
