265ff70779654547582ac2368d632edec117f9a5
kay0u <pierre.moltess@gmail.com>
2019-06-27 12:09:48 +0200

    12.0.2

--- scripts/upgrade.d/upgrade.last.sh
+++ scripts/upgrade.d/upgrade.last.sh
@@ -1,7 +1,7 @@
-gitlab_version="12.0.0"
+gitlab_version="12.0.2"
 
-gitlab_x86_64_source_sha256="b32070d2a4c83c914b9fd07713501e3c99e31a288de64a7794a6de2c2b6f7494"
+gitlab_x86_64_source_sha256="8c528a01ceca69ba566161b3d06a1f5315f76b3468f7b2c411bf830fcc486099"
 
-gitlab_arm_source_sha256="cd3f0ffd16d1d80184e79b552ae5cc3bb33012d1d51bb9c5cb48d461f5c70b07"
+gitlab_arm_source_sha256="6953194c3939b606831399c6728dc168b5e82faf9db088f976f515900d534b74"
 
 gitlab_filename="gitlab-ce-${gitlab_version}.deb"
