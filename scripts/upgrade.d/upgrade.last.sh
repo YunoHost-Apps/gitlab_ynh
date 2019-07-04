7931d8872592427b98d1c81c9932a96b61e74f92
Pierre Bourré <pierre.moltess@gmail.com>
2019-07-04 20:30:29 +0200

    Actions are different between 11.x and 12.x

--- scripts/upgrade.d/upgrade.last.sh
+++ scripts/upgrade.d/upgrade.last.sh
@@ -5,3 +5,8 @@ gitlab_x86_64_source_sha256="9e2dcb0e4e3ca3c230d43c6b8580d77c2c716f3c6d1055739f1
 gitlab_arm_source_sha256="992710fd2c4076961f03633cb6b56331f7ade63a6869c47c2ce28995f8b43893"
 
 gitlab_filename="gitlab-ce-${gitlab_version}.deb"
+
+# Action to do in case of failure of the package_check
+package_check_action() {
+		ynh_replace_string --match_string="command \"sysctl -e --system\"" --replace_string="command \"sysctl -e --system || true\"" --target_file="$final_path/embedded/cookbooks/package/resources/sysctl.rb"
+}
