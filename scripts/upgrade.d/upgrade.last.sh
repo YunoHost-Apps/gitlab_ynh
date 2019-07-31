556fefd525ad1e3e6a2187b4b5631e3766f55579
Kayou <pierre.moltess@gmail.com>
2019-07-31 17:49:20 +0200

    Fix package_check_action

--- scripts/upgrade.d/upgrade.last.sh
+++ scripts/upgrade.d/upgrade.last.sh
@@ -8,5 +8,9 @@ gitlab_filename="gitlab-ce-${gitlab_version}.deb"
 
 # Action to do in case of failure of the package_check
 package_check_action() {
-		ynh_replace_string --match_string="command \"sysctl -e --system\"" --replace_string="command \"sysctl -e --system || true\"" --target_file="$final_path/embedded/cookbooks/package/resources/sysctl.rb"
+	local sysctl_file="$final_path/embedded/cookbooks/package/resources/gitlab_sysctl.rb"
+	if [ ! -f "$sysctl_file" ]; then
+		sysctl_file="$final_path/embedded/cookbooks/package/resources/sysctl.rb"
+	fi
+	ynh_replace_string --match_string="command \"sysctl -e --system\"" --replace_string="command \"sysctl -e --system || true\"" --target_file=$sysctl_file
 }
