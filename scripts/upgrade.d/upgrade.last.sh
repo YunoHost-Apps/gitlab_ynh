3e4b978e725f44a482e15a26aab38e5bb6c033ef
Kay0u <pierre@kayou.io>
2020-04-22 21:53:25 +0200

    fix install

--- scripts/upgrade.d/upgrade.last.sh
+++ scripts/upgrade.d/upgrade.last.sh
@@ -18,8 +18,8 @@ gitlab_filename="gitlab-ce-${gitlab_version}.deb"
 # Action to do in case of failure of the package_check
 package_check_action() {
 	local sysctl_file="$final_path/embedded/cookbooks/package/resources/gitlab_sysctl.rb"
-	if [ ! -f "$sysctl_file" ]; then
-		sysctl_file="$final_path/embedded/cookbooks/package/resources/sysctl.rb"
-	fi
-	ynh_replace_string --match_string="command \"sysctl -e --system\"" --replace_string="command \"sysctl -e --system || true\"" --target_file=$sysctl_file
+	ynh_replace_string --match_string="command \"sysctl -e \(.*\)\"" --replace_string="command \"sysctl -e \1 || true\"" --target_file=$sysctl_file
+	
+	sysctl_file="/opt/gitlab/embedded/cookbooks/package/recipes/sysctl.rb"
+	ynh_replace_string --match_string="command \"sysctl -e \(.*\)\"" --replace_string="command \"sysctl -e \1 || true\"" --target_file=$sysctl_file
 }
