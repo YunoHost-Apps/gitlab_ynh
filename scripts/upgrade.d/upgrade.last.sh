5f2544a392b7e5760fd6075884c98a5a2df84fd0
Kay0u <pierre@kayou.io>
2021-09-06 13:42:11 +0200

    14.2.3

--- scripts/upgrade.d/upgrade.last.sh
+++ scripts/upgrade.d/upgrade.last.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-gitlab_version="14.2.2"
+gitlab_version="14.2.3"
 
 # sha256sum found here: https://packages.gitlab.com/gitlab
 gitlab_debian_version="buster"
 
-gitlab_x86_64_buster_source_sha256="77de4f76b093c5a849def556c7fa8b9253efee340ad8a79fae2b5e20fd2b4b56"
+gitlab_x86_64_buster_source_sha256="c83a3f9d73e4b2178b13a856aa41bd7eea5319c0bddec4cc50f29e294e02adda"
 
-gitlab_arm64_buster_source_sha256="a468eb0c9220cf7975fda0cecf949a9fad969c621b3408c01f79b4825ebfc0c1"
+gitlab_arm64_buster_source_sha256="b5980a570c62943b744ec71e5124e3efd88418331edc4f2dde4a14bcefa8b52e"
 
-gitlab_arm_buster_source_sha256="06041dad5b4697384e6920a737a2b7ba1c730ef540941162075cc8c4c8e8ecff"
+gitlab_arm_buster_source_sha256="323e9c182a95c3f67335907426dc9e917f8a065b4da4014053a84c85b190ee8f"
 
 architecture=$(ynh_app_setting_get --app="$app" --key=architecture)
 
@@ -20,8 +20,8 @@ elif [ "$architecture" = "arm64" ]; then
 elif [ "$architecture" = "arm" ]; then
 	# If the version for arm doesn't exist, then use an older one
 	if [ -z "$gitlab_arm_buster_source_sha256" ]; then
-		gitlab_version="14.2.2"
-		gitlab_arm_buster_source_sha256="06041dad5b4697384e6920a737a2b7ba1c730ef540941162075cc8c4c8e8ecff"
+		gitlab_version="14.2.3"
+		gitlab_arm_buster_source_sha256="323e9c182a95c3f67335907426dc9e917f8a065b4da4014053a84c85b190ee8f"
 	fi
 	gitlab_source_sha256=$gitlab_arm_buster_source_sha256
 fi
