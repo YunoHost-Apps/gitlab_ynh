971dc8f94fe0b5891da04f61192428bb00617375
Kay0u <pierre@kayou.io>
2022-02-24 19:05:26 +0100

    14.8.1

--- scripts/upgrade.d/upgrade.last.sh
+++ scripts/upgrade.d/upgrade.last.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-gitlab_version="14.8.0"
+gitlab_version="14.8.1"
 
 # sha256sum found here: https://packages.gitlab.com/gitlab
 gitlab_debian_version="$(lsb_release -sc)"
 
-gitlab_x86_64_bullseye_source_sha256="defaf15ae7ab2a901d7be1a1b97afe6d849c1eff7b2ad7df184eb9971ebd2263"
-gitlab_x86_64_buster_source_sha256="ce503bf0ff42d2499358945115ad557f15a3a60a39c166776d4db76a1044bcc9"
+gitlab_x86_64_bullseye_source_sha256="e544887406a79d4f3545eb8b84c3c9fa2a0c016481ee33d6a1db84561aff8b65"
+gitlab_x86_64_buster_source_sha256="b89d8fa4a31703377951de663b73062ac22f213c9ec1fe2821027558ad496265"
 
-gitlab_arm64_bullseye_source_sha256="fe2fa71f00239cba7ed43b5482969ba8cbcb10fe89199bd8d5ee607699007bdb"
-gitlab_arm64_buster_source_sha256="7b29a329fa1123fa97d1f299149bc5ce5f52db8b2a3bb8084bc8800507740f0f"
+gitlab_arm64_bullseye_source_sha256="c1163c372b0a2bfa61e5275a75df46ef7c365b737a37227a79df2a64bad3ad17"
+gitlab_arm64_buster_source_sha256="7c5e2e4d3a784888d3aba2a30e49b7d55a1ada77ae3e4ee1619f8d1e792fe7e0"
 
-gitlab_arm_buster_source_sha256="eca82d58a7b6d2c308ec4fcccf8d176ece5508a2ec5297bd3dfcd826135a762d"
+gitlab_arm_buster_source_sha256="c744081368656d6a76b48fc148a528b32b08facc58b74748ef1e301878b08f47"
 
 architecture=$(ynh_app_setting_get --app="$app" --key=architecture)
 
@@ -35,8 +35,8 @@ elif [ "$architecture" = "arm64" ]; then
 elif [ "$architecture" = "arm" ]; then
 	# If the version for arm doesn't exist, then use an older one
 	if [ -z "$gitlab_arm_buster_source_sha256" ]; then
-		gitlab_version="14.8.0"
-		gitlab_arm_buster_source_sha256="eca82d58a7b6d2c308ec4fcccf8d176ece5508a2ec5297bd3dfcd826135a762d"
+		gitlab_version="14.8.1"
+		gitlab_arm_buster_source_sha256="c744081368656d6a76b48fc148a528b32b08facc58b74748ef1e301878b08f47"
 	fi
 	gitlab_source_sha256=$gitlab_arm_buster_source_sha256
 fi
