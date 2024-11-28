aa68465459f18c82345d08b19ac2463ba2348929
Kay0u <pierre@kayou.io>
2024-11-28 09:39:17 +0100

    17.6.1

--- scripts/upgrade.d/upgrade.last.sh
+++ scripts/upgrade.d/upgrade.last.sh
@@ -1,22 +1,20 @@
 #!/bin/bash
 
-gitlab_version="17.5.2"
+gitlab_version="17.6.1"
 
 # Upgrade path: https://gitlab-com.gitlab.io/support/toolbox/upgrade-path/
 # sha256sum found here: https://packages.gitlab.com/gitlab
 gitlab_debian_version="$(lsb_release -sc)"
 
-gitlab_x86_64_bookworm_source_sha256="93071122d7d559c6372770d41626e2d03b3af4be5651a3cb669505cd6d8a3b00"
-gitlab_x86_64_bullseye_source_sha256="4d98e59751a323067345e6adbbe9ce6472acb1f670f09b30fc3aa6be21f1ae4a"
-gitlab_x86_64_buster_source_sha256="239d800894a1d447142be53babc25c480ce9ed0048376b71a9b973c8add1c651"
+gitlab_x86_64_bookworm_source_sha256="6a7a7c5388fbf96333e1934e191d23c116c727c5bc0a47c7a432563442a7efd1"
+gitlab_x86_64_bullseye_source_sha256="8e37b5ba22d86e7d066e9fc984460d71f9560c5907799e1d06c9030de316aa81"
 
-gitlab_arm64_bookworm_source_sha256="ba319f7a8c2ac117bf3ea781a4bd49e86f420ded66ffd553e074016a8d9e13f1"
-gitlab_arm64_bullseye_source_sha256="cec95e0dcbf342bbd66bf64d46fad2df02dcc7ac704e8715f80026d178b33dbd"
-gitlab_arm64_buster_source_sha256="513f576deb53a2a96455bceed5e05cf646fda3ee04220e4c2ea6dcaca1d0886a"
+gitlab_arm64_bookworm_source_sha256="dbb7dfa4194eeaed0039d6bb8613a2557ee622c94c8fb9a988fdc9fbf702e653"
+gitlab_arm64_bullseye_source_sha256="f2a44624b8e75a5dbe1464f6a7f3323ea755480d9681547bb921febd01eaf029"
 
-gitlab_arm_bookworm_source_sha256="5f2e559f70d99cb1e8c7ec7b57d20e8b7a79e94d73d5315c6a9d448423e8d2de"
-gitlab_arm_bullseye_source_sha256="952fc4d0abbe5e1a697fbdab7f287c2613f3f35879b07cf24099a5edf4394470"
-gitlab_arm_buster_source_sha256="6cfbec4a328ff4d7db23608eab2feaff6289a978b9775eb0bbd6e77775fc508b"
+gitlab_arm_bookworm_source_sha256="e9f9592123602e7864b6aa164c27cc2d9a23288e0897b1f5ddc811f2d50cd9b2"
+gitlab_arm_bullseye_source_sha256="3c267ce185b7b9a96fbf2aedbdd96a9fde23ea7187cb99b6cdc3ad4ca52957f3"
+gitlab_arm_buster_source_sha256="dc5051a10bc9ee2d1277cda59084af25bdd12eb74997820cac658b2403ad519e"
 
 architecture=$(ynh_app_setting_get --app="$app" --key=architecture)
 
@@ -31,9 +29,6 @@ if [ "$architecture" = "x86-64" ]; then
 	elif [ "$gitlab_debian_version" = "bullseye" ]
 	then
 		gitlab_source_sha256=$gitlab_x86_64_bullseye_source_sha256
-	elif [ "$gitlab_debian_version" = "buster" ]
-	then
-		gitlab_source_sha256=$gitlab_x86_64_buster_source_sha256
 	fi
 elif [ "$architecture" = "arm64" ]; then
 	if [ "$gitlab_debian_version" = "bookworm" ]
@@ -42,9 +37,6 @@ elif [ "$architecture" = "arm64" ]; then
 	elif [ "$gitlab_debian_version" = "bullseye" ]
 	then
 		gitlab_source_sha256=$gitlab_arm64_bullseye_source_sha256
-	elif [ "$gitlab_debian_version" = "buster" ]
-	then
-		gitlab_source_sha256=$gitlab_arm64_buster_source_sha256
 	fi
 elif [ "$architecture" = "arm" ]; then
 	if [ "$gitlab_debian_version" = "bookworm" ]
