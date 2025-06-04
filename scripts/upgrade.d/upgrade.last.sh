b51f862970623883050c8d1ef704373cef3b78ff
eric_G <46165813+ericgaspar@users.noreply.github.com>
2025-06-04 13:30:07 +0200

    Update upgrade.last.sh

--- scripts/upgrade.d/upgrade.last.sh
+++ scripts/upgrade.d/upgrade.last.sh
@@ -6,14 +6,14 @@ gitlab_version="18.0.1"
 # sha256sum found here: https://packages.gitlab.com/gitlab
 gitlab_debian_version="$(lsb_release -sc)"
 
-gitlab_x86_64_bookworm_source_sha256="01dd05efa98d278ed6178ab449ae477d2e70f552ceba4825c8e24b7572e20a79"
-gitlab_x86_64_bullseye_source_sha256="24ad242b559e3d8c432a082da1c0e1652263cca3ce065e0a819ddc3dadeceecf"
+gitlab_amd64_bookworm_source_sha256="01dd05efa98d278ed6178ab449ae477d2e70f552ceba4825c8e24b7572e20a79"
+gitlab_amd64_bullseye_source_sha256="24ad242b559e3d8c432a082da1c0e1652263cca3ce065e0a819ddc3dadeceecf"
 
 gitlab_arm64_bookworm_source_sha256="72f80ce75344da0d5ba22301d57a24041195fd550902b719298e496ae8591516"
 gitlab_arm64_bullseye_source_sha256="31075a63ce82f9f6d3b2a82d4fd61b0d543e63a3f1dd3bdca48a915d314e8b86"
 
-gitlab_arm_bookworm_source_sha256=""
-gitlab_arm_bullseye_source_sha256=""
+gitlab_armhf_bookworm_source_sha256=""
+gitlab_armhf_bullseye_source_sha256=""
 
 architecture=$(ynh_app_setting_get --app="$app" --key=architecture)
 
